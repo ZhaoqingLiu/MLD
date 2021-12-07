58eabcf2da4f4560f14e14d09190fe53bdff85a4
ZhaoqingLiu <Zhaoqing.Liu-1@student.uts.edu.au>
2021-12-07 21:58:46 +1100

    Add FEO-FFE method

---
+++ afdt/feo_ffe.py
@@ -0,0 +1,15 @@
+"""
+@author: Zhaoqing Liu
+@email : Zhaoqing.Liu-1@student.uts.edu.au
+@desc  :
+"""
+"""
+Code ref.:
+1. https://pypi.org/project/scikit-fuzzy/ and https://github.com/scikit-fuzzy/scikit-fuzzy
+2. https://pypi.org/project/fuzzy-c-means/ and https://github.com/omadson/fuzzy-c-means
+"""
+import numpy as np
+
+
+if __name__ == '__main__':
+    print("Fuzzy Feature Extraction")
