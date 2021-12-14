4eeb2e1a804ea49c35dcc98961e885087045f92e
ZhaoqingLiu <Zhaoqing.Liu-1@student.uts.edu.au>
2021-12-14 14:50:49 +1100

    Update

--- adafdt/ada_fdt.py
+++ adafdt/ada_fdt.py
@@ -1,10 +1,7 @@
 """
 @author: Zhaoqing Liu
 @email : Zhaoqing.Liu-1@student.uts.edu.au
-@desc  : Fuzzy Feature Selection algorithm
-"""
-"""
-mRMR feature selection:
+@desc  :
 """
 
 
--- adafdt/feo_ffe.py
+++ adafdt/feo_ffe.py
@@ -1,15 +1,13 @@
 """
 @author: Zhaoqing Liu
 @email : Zhaoqing.Liu-1@student.uts.edu.au
-@desc  :
+@desc  : Fuzzy Feature Extraction algorithm
 """
 """
 Code ref.:
 1. https://pypi.org/project/scikit-fuzzy/ and https://github.com/scikit-fuzzy/scikit-fuzzy
 2. https://pypi.org/project/fuzzy-c-means/ and https://github.com/omadson/fuzzy-c-means
 """
-import numpy as np
 
 
-if __name__ == '__main__':
-    print("Fuzzy Feature Extraction")
+
